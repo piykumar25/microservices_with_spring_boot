17396215327cff6239d8ea8372326f6fcb1e6718
piykumar25 <piykumar05i@gmail.com>
2024-10-05 12:26:02 +0530

    Code changes for service discovery and service registration

---
+++ eurekaserver/src/test/java/com/myBank/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myBank.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
